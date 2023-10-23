1d90d4ce9e5d83c342838484b5cdb375ef9d8b93
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2023-10-23 12:06:04 +0600

    Add files via upload

---
+++ 11 class_Type Function & String Formating.py
@@ -0,0 +1,26 @@
+# abcd = egula string; 1234= dosomik chara egula intiger; 12.4= eta float
+
+#Type Function :
+num=123
+print (type (num))
+
+num=123.556
+print (type (num))
+
+num= "sas"
+print (type (num))
+
+
+#String Formating : 
+num1=10
+num2=40
+print(num1+num2)
+#formating:
+print(f"{num1}+{num2}={num1+num2}")
+
+
+#new line print na kore ek linei print korte :
+
+print("Shameem",end=" ") #end er moddhe ja likhbo tai print korbe
+print("Hacker")
+
---
+++ 12 class_Relational Operator & Boolean Data Type.py
@@ -0,0 +1,10 @@
+#Relational Operator & Boolean Data Type
+
+print(30>20)
+print(30<20)
+print(30>=20)
+print(30<=20)
+print(30!=20)
+print(30==20)
+print("SAS"=="SAS")
+print("SAS"=="BD")
