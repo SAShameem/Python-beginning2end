a33a93db3dfc8a26f8e0b8f33fccbb7434d0b3b6
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2023-11-14 13:49:58 +0600

    Add files via upload

---
+++ 22 class_ Break & Continue.py
@@ -0,0 +1,18 @@
+# use of Break (kaj: loop theke beriye jaoya)
+i=1
+while i<=20:
+    if i==20:
+        break
+    print(i)
+    i=i+1
+print("Hello")
+
+# use of Continue (kaj: loop e fire jaoya)
+i=1
+while i<=20:
+    if i==20:
+        continue
+    print(i)
+    i=i+1
+print("Hello")
+
