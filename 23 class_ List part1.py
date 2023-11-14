1137d526110eebc3f4d2b7cb23602813e65138a6
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2023-11-14 13:50:20 +0600

    Add files via upload

---
+++ 23 class_ List part1.py
@@ -0,0 +1,23 @@
+subjects = ["C", "C++", "Java", "Python", "BD", "IN"]
+
+print(subjects)
+print(subjects[0])
+print(subjects[5])
+print(subjects[2:])
+print(subjects[-1])
+
+#Uses of in & not in :
+
+#checking from subjects
+print("Python" in subjects)
+print("sas" in subjects)
+print("sas"not in subjects)
+
+# add something on list
+print(subjects+ ["HTML",203])
+print(subjects*3)
+
+
+
+
+
