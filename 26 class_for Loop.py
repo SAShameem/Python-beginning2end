9ff57fdd00bff27c0880b18706cbd136cddbc3ea
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2023-11-15 18:58:41 +0600

    Add files via upload

---
+++ 26 class_for Loop.py
@@ -0,0 +1,20 @@
+num = [10,20,30,40,50]
+print(num)
+
+for x in num:
+    print(x)
+    
+#sum:
+sum=0
+for x in num:
+    sum=sum+x
+print(sum)
+
+
+index=0
+#n=len(num)
+while index<5: # or, index<n: 
+    print(num[index])
+    index=index+1
+
+
