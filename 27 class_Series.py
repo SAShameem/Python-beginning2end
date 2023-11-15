24f9fc0dc8e51940691ffd815ac277ca7cf2b901
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2023-11-15 18:59:55 +0600

    Add files via upload

---
+++ 27 class_Series.py
@@ -0,0 +1,45 @@
+#1+2+3+..........+n
+n=int(input("Enter the last number : "))
+
+sum=0
+for x in range (1,n+1,1):
+    sum=sum+x
+print(sum)
+
+
+#2+4+6+..........+n
+n=int(input("Enter the last number : "))
+
+sum=0
+for x in range (2,n+1,2):
+    sum=sum+x
+print(sum)
+
+
+#1+3+5+..........+n
+n=int(input("Enter the last number : "))
+
+sum=0
+for x in range (1,n+1,2):
+    sum=sum+x
+print(sum)
+
+
+#4+8+12+..........+n
+n=int(input("Enter the last number : "))
+
+sum=0
+for x in range (4,n+1,4):
+    sum=sum+x
+print(sum)
+
+
+#1^2+2^2+3^2+..........+n^2
+#1*1+2*2+3*3+....+n*n (simple format)
+n=int(input("Enter the last number : "))
+
+sum=0
+for x in range (1,n+1,1):
+    sum=sum+x*x
+print(sum)
+
