485d9c0f03a54a59712d4b5998d44ec48e18ac38
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-07 00:21:15 +0600

    Add files via upload

---
+++ 34 class1_Set.py
@@ -0,0 +1,20 @@
+num1 = {1,2,3,4,5,5}
+num2 = set([4,5,6,7])
+
+print(num2)
+print(num1)
+
+# set e add korte:
+num1.add(88)
+print(num1)
+
+# Add, Remove:
+num2.add(9)
+num2.remove(5)
+print(num2)
+
+
+# Num Check:
+print(4 in num2)
+print(4 not in num2)
+
