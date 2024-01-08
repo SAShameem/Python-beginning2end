543895505a440286a59d89278689038d61cda564
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-08 21:25:38 +0600

    Add files via upload

---
+++ 34 class2_Set(Union,Intersection,Difference).py
@@ -0,0 +1,11 @@
+num1 = {1,2,3,4,5}
+num2 = {4,5,6,7,8,9}
+
+#Union (all):
+print(num1 | num2)
+
+#Intersection (only commons):
+print(num1 & num2)
+
+#Difference [num1 - (common from num2)] :
+print(num1 - num2)
