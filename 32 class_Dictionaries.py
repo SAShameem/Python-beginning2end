3d674db980274aebb5c5929c6587ffa480ec3890
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-08 21:24:29 +0600

    Add files via upload

---
+++ 32 class_Dictionaries.py
@@ -0,0 +1,14 @@
+StudentID = {
+    
+    "103337" : "Smeem",
+    "103336" : "Kamru",
+     103335 : "amin",
+     103334 : "abcd"
+}    
+    
+print(StudentID["103337"])
+
+print(StudentID.get(103334))
+
+print(StudentID.get("103339","Not a valid key"))
+
