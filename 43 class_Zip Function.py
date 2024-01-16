7ab2838b9bc1c79837ce15f8d34b3cfff0a560d4
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-16 15:06:47 +0600

    Add files via upload

---
+++ 43 class_Zip Function.py
@@ -0,0 +1,6 @@
+print("Students Who Got GPA 5 in HSC: #")
+
+roll = [101,105,106,109,111,134,153,199,200,420]
+name = ["SAMIM","RAMIM","MONIR","NASIM","SAMIUL","SULAIMAN","ROBIUL","NASIR","HAFIZUR","RAJOB"]
+# print(list(zip(roll,name)))
+print(list(zip(roll,name,"ABCDABCDAB")))
