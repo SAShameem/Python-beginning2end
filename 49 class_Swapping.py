eafaea0f80940a04e9969bb9987d7728cab1dc07
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-18 22:58:22 +0600

    Add files via upload

---
+++ 49 class_Swapping.py
@@ -0,0 +1,19 @@
+## Swapping on C/C++
+a = 33
+b = 80
+temp = a #temp =33
+a=b #a=80
+b=temp #b=33
+
+print("a =",a)
+print("b =",b)
+
+print("========-----========")
+
+## Swapping on Python
+
+a = 33
+b = 80
+a,b = b,a
+print("a =",a)
+print("b =",b)
