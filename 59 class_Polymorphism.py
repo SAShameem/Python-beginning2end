73f525e207bbc273ba30f3437d4ec8e651c57d60
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-29 23:57:41 +0600

    Add files via upload

---
+++ 59 class_Polymorphism.py
@@ -0,0 +1,11 @@
+### Polymorphism (bohurup)
+
+# Built in Polymorphic Function:
+print(len("SAMIM"))
+print(len([10,20,30]))
+
+# User Defined Function:
+def add(x, y, z=0):
+    return x + y +z
+print(add(2,3))
+print(add(2,3,4))
