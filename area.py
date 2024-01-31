2c84ba5a105e5a8be411e614b9616e6f80a25278
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-01-31 19:38:54 +0600

    61class

---
+++ area.py
@@ -0,0 +1,5 @@
+def triangle_area(b,h):
+    print(f"Area Of Triangle: "{.5*b*h})
+    
+def rectangle_area(a,b):
+    print(f"Area Of Rectangle: "{a*b})
---
+++ test.py
@@ -0,0 +1,17 @@
+from area import triangle_area,rectangle_area
+
+triangle_area(10,20)
+rectangle_area(10,20)
+
+from area import *
+
+triangle_area(10,20)
+rectangle_area(10,20)
+
+
+
+from math import pow #Only POW Library
+print(pow(2,3))
+ 
+from math import * # Toimport all Library (eta hudai use kora uchit na)
+print(pow(2,3))
